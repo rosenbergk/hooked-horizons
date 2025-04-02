51b14f5c951335a77f082a92864361653bc472b2
rosenbergk <ktros49@gmail.com>
2025-04-02 15:27:10 +0200

    Created a first person camera on the boat. Camera rotates with mouse movement

---
+++ FishingGame/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
