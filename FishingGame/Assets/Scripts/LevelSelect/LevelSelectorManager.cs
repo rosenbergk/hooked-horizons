b9d5dd690ad00b565cd23bca16a74e356c0e3ee6
danielmansueto <dmansueto30@gmail.com>
2025-04-29 15:45:48 +0200

    UI changes to menu, level select and fishing scene

--- FishingGame/Assets/Scripts/LevelSelect/LevelSelectorManager.cs
+++ FishingGame/Assets/Scripts/LevelSelect/LevelSelectorManager.cs
@@ -4,11 +4,33 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectorManager : MonoBehaviour
 {
+    [SerializeField]
+    private string openWaterSceneName = "OpenWater"; 
+    [SerializeField]
+    private string timedModeSceneName = "TimedMode";
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            StartTimedMode();
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            StartOpenWater();
+        }
+    }
     public void StartOpenWater()
     {
         GameManager.Instance.StartGame();
         SceneManager.LoadScene("OpenWater");
     }
+
+        public void StartTimedMode()
+    {
+        GameManager.Instance.StartGame();
+        SceneManager.LoadScene("OpenWater");
+    }
+
 
     public void BackToMainMenu()
     {
