f74948cbe77e72894b6d35bab7f747f1026130e9
rosenbergk <ktros49@gmail.com>
2025-05-02 12:02:59 +0200

    Top fish panel now opens after playing a game

--- FishingGame/Assets/Scripts/TopFishPanelManager.cs
+++ FishingGame/Assets/Scripts/TopFishPanelManager.cs
@@ -19,6 +19,8 @@ public class TopFishPanelController : MonoBehaviour
     /// </summary>
     public void TogglePanel()
     {
+        Debug.Log("[Stats] TogglePanel called!  Panel currently active = " + panel.activeSelf);
+
         bool nowOn = !panel.activeSelf;
         panel.SetActive(nowOn);
         if (nowOn)
